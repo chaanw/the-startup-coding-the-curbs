ca5bc1413525a6780db8804fe70267bcca1de90c
ju5tu5 <justus@ju5tu5.nl>
2022-01-17 14:31:33 +0100

    Update script.js

--- scripts/script.js
+++ scripts/script.js
@@ -1,26 +1,35 @@
-let container = document.querySelector(".container");
-let maskContainer = document.querySelector(".mask-container");
-let maskImageContainer = document.querySelector(".mask-image-container");
+let container = document.querySelector('.container')
+let maskContainer = document.querySelector('.mask-container')
+let maskImageContainer = document.querySelector('.mask-image-container')
 
-let border = document.querySelector(".border");
-let circle = document.querySelector(".circle");
+let border = document.querySelector('.border')
+let circle = document.querySelector('.circle')
 
-circle.style.draggable = true;
+circle.style.draggable = true
 
-circle.ondrag = function(event){
-    console.log('dragging')
-    maskContainer.style.width = event.pageX + "px";
-    border.style.left = event.pageX + "px";
-    circle.style.left = event.pageX + "px";
+circle.addEventListener('mousedown', () => {
+  circle.addEventListener('mousemove', moveSlider)
+  circle.addEventListener('mouseup', () => {
+    circle.removeEventListener('mousemove', moveSlider)
+  })
+})
+
+function moveSlider(event) {
+  maskContainer.style.width = event.pageX + 'px'
+  border.style.left = event.pageX + 'px'
+  circle.style.left = event.pageX + 'px'
 }
 
-circle.ondragend = function(event){
-    console.log('end dragging')
-    maskContainer.style.width = event.pageX + "px";
-    border.style.left = event.pageX + "px";
-    circle.style.left = event.pageX + "px";
-}
+// circle.ondrag = function(event){
+//     console.log('dragging')
+//     maskContainer.style.width = event.pageX + "px";
+//     border.style.left = event.pageX + "px";
+//     circle.style.left = event.pageX + "px";
+// }
 
-
-
-
+// circle.ondragend = function(event){
+//     console.log('end dragging')
+//     maskContainer.style.width = event.pageX + "px";
+//     border.style.left = event.pageX + "px";
+//     circle.style.left = event.pageX + "px";
+// }
